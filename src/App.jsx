676c2208afdca5f93ce0402afce7aeccbc01c8e8
Francisco Pereira <80694995+franciscopereira32@users.noreply.github.com>
2024-10-22 16:53:36 +1030

    update navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import { Navbar } from "./components/Navbar/Navbar";
 import styles from "./App.module.css";
+import { Navbar } from "./components/Navbar/Navbar";
 
 function App() {
-  return <div className={styles.App}>< Navbar/></div>
+  return <div className={styles.App}>< Navbar/ ></div>
 }
 
 export default App;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,26 +1,35 @@
-import React from "react";
+import React, {useState} from "react";
 import styles from "./Navbar.module.css";
+import { getImageUrl } from "../../utils";
 
 export const Navbar = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
     return (
-        <nav className={Styles.navbar}>
+        <nav className={styles.navbar}>
             <a className={styles.title} href="/">Portfolio</a>
-        {/* <div className={styles.menu}>
-                <ul className={styles.menuItems}>
-                    <li> 
-                        <a href="#about">About</li></a>
-                    </li>
-                    <li> 
-                        <a href="#experience">Experience</li></a>
-                    </li>
-                    <li> 
-                        <a href="#projects">Projects</li></a>
-                    </li>
-                    <li> 
-                    <a href="#contact">Contact</li></a>
-                </li>
-                </ul>
-            </div>  */}
+                <div className={styles.menu}>
+                    <img className={styles.menuBtn} src={
+                        menuOpen 
+                        ? getImageUrl("nav/closeIcon.png") 
+                        : getImageUrl("nav/menuIcon.png") 
+                    } 
+                        alt="menu-button"
+                        onClick={() => setMenuOpen(!menuOpen)}/> 
+                        <ul className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`}>
+                               {/* <li> 
+                                    <a href="#about">About</li></a>
+                                </li>
+                                <li> 
+                                    <a href="#experience">Experience</li></a>
+                                </li>
+                                <li> 
+                                    <a href="#projects">Projects</li></a>
+                                </li>
+                                <li> 
+                                <a href="#contact">Contact</li></a>
+                            </li> */}
+                        </ul>
+                </div>  
         </nav>
     )    
 };

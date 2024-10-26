8c570dbfe641cbf6826e2ca3429590b366f59eeb
Francisco Pereira <80694995+franciscopereira32@users.noreply.github.com>
2024-10-27 01:28:35 +1030

    update about and fix nvbar

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -4,13 +4,13 @@ import { getImageUrl } from "../../utils";
 
 export const About = () => {
     return ( 
-    <section className={styles.container}>
+    <section className={styles.container} id="about">
         <h2 className={styles.title}>About</h2>
         <div className={styles.content}><img src={getImageUrl("about/aboutImage.png")}
         alt= "Me sitting with a laptop" className={styles.aboutImage}
         />
         <ul className={styles.aboutItems}>
-            <li className={styles.aboutItems}>
+            <li className={styles.aboutItem}>
                 <img src={getImageUrl('about/cursorIcon.png')} 
                 alt="cursor icon" />
                     <div className={styles.aboutItemText}>
@@ -18,7 +18,7 @@ export const About = () => {
                     <p>I am a software analyst with experience in Android documentation and project management.</p>
                 </div>
             </li>
-            <li className={styles.aboutItems}>
+            <li className={styles.aboutItem}>
                 <img src={getImageUrl('about/serverIcon.png')} 
                 alt="server icon" />
                     <div className={styles.aboutItemText}>
@@ -26,7 +26,7 @@ export const About = () => {
                     <p>I have experience as a Java developer specializing in test automation, as well as customizing and setting up Android devices.</p>
                 </div>
             </li>
-            <li className={styles.aboutItems}>
+            <li className={styles.aboutItem}>
                 <img src={getImageUrl('about/uiIcon.png')} 
                 alt="UI icon" />
                     <div className={styles.aboutItemText}>
@@ -34,7 +34,7 @@ export const About = () => {
                     <p>I have experience in bug debugging within Unix architecture, as well as using Android Debug Bridge (ADB) commands and Unix commands.</p>
                 </div>
             </li>
-            <li className={styles.aboutItems}>
+            <li className={styles.aboutItem}>
                 <img src={getImageUrl('about/settingsIcon.png')} 
                 alt="settings icon" />
                     <div className={styles.aboutItemText}>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,34 +3,35 @@ import styles from "./Navbar.module.css";
 import { getImageUrl } from "../../utils";
 
 export const Navbar = () => {
-    const [menuOpen, setMenuOpen] = useState(false);
+    const [menuOpen, setMenuOpen] = useState(false); 
     return (
         <nav className={styles.navbar}>
             <a className={styles.title} href="/">Portfolio</a>
                 <div className={styles.menu}>
                     <img className={styles.menuBtn} src={
                         menuOpen 
-                        ? getImageUrl("nav/closeIcon.png") 
-                        : getImageUrl("nav/menuIcon.png") 
+                            ? getImageUrl("nav/closeIcon.png") 
+                            : getImageUrl("nav/menuIcon.png") 
                     } 
                         alt="menu-button"
                         onClick={() => setMenuOpen(!menuOpen)}/> 
-                        <ul className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`}
+                        <ul 
+                        className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`}
                         onClick={() => setMenuOpen(false)}>
-                               {/* <li> 
-                                    <a href="#about">About</li></a>
+                            <li> 
+                                    <a href="#about">About</a>
                                 </li>
                                 <li> 
-                                    <a href="#experience">Experience</li></a>
+                                    <a href="#experience">Experience</a>
                                 </li>
                                 <li> 
-                                    <a href="#projects">Projects</li></a>
+                                    <a href="#projects">Projects</a>
                                 </li>
                                 <li> 
-                                <a href="#contact">Contact</li></a>
-                            </li> */}
+                                <a href="#contact">Contact</a>
+                            </li> 
                         </ul>
                 </div>  
         </nav>
-    )    
+    );    
 };

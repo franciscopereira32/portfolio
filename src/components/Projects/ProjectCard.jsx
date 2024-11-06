a92d750814c9b7f275c29c8b89738622705b81eb
Francisco Pereira <80694995+franciscopereira32@users.noreply.github.com>
2024-11-06 13:23:12 +1030

    ridden description

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -1,17 +1,35 @@
 import React from "react";
 import { getImageUrl } from "../../utils";
 import styles from "./ProjectCard.module.css";
+import { useState } from "react";
 
 export const ProjectCard = ({ 
     project : { title, imageSrc, description, skills, demo, source} },
 ) => {
+
+    const [showElement, setShowElement] = useState(false)
+    // const showOrHide = () => setShowElement(true)
+
+    const toggleElement = () => {
+        setShowElement(!showElement);
+      };
+    
+      const elementStyle = {
+        display: showElement ? 'block' : 'none'
+      };
+
     return (
         <div className={styles.container}>
             <img 
                 src={getImageUrl(imageSrc)} 
                 alt={`Image of ${title}`} className={styles.image} />
                 <h3 className={styles.title}>{title}</h3>
-                <p className={styles.description}>{description}</p>
+                <div>
+                    <button onClick={toggleElement} className={styles.descriptionBtn}>Description</button>
+                    <div style={elementStyle}>
+                        <p className={styles.description}>{description}</p>
+                    </div>
+                </div>
                 <ul className={styles.skills}>
                     {skills.map((skill, id) => {
                     return (
